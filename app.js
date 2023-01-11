213353260e7d704d7961080d1b8b7d5945e16674
CARLOS TUMBACO <carlostumbaco1996@hotmail.com>
2023-01-10 20:11:17 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+ document.addEventListener('DOMContentLoaded',() => {/*Espero
+      que se ejecute todo el código hmtl y luego se procede
+      a ejecutar el javascript */
+
+    holamundo();
+
+
+ });
+
+
+function holamundo(){
+
+    alert('hola mundo');
+    
+}
